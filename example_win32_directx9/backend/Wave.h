a0ff2322643138e3e3520a2f93269245f2c3428a
FramGM <71141393+FramGM@users.noreply.github.com>
2026-03-11 11:51:19 +0300

    Update Wave.h

--- example_win32_directx9/backend/Wave.h
+++ example_win32_directx9/backend/Wave.h
@@ -1,21 +1,33 @@
 #pragma once
-#include "WaveData.h"
+
+struct WaveData
+{
+	float X, Amp, Freq, Offset;
+	float m_dlTimeDiff;
+	WaveData(float x, float amp, float freq, float offset, float dlTimeDiff) { X = x; Amp = amp; Freq = freq; Offset = offset; m_dlTimeDiff = dlTimeDiff; }
+};
+
+#include "WaveFunc.h"
 
 class Wave
 {
 public:
-	Wave(double dlStartPosX, double dlAmplitude, double dlFrequency, double dlOffset, double dlStartTime) : m_WaveData(dlStartPosX, dlAmplitude, dlFrequency, dlOffset, dlStartTime) {};
+	Wave(float dlStartPosX, float dlAmplitude, float dlFrequency, float dlOffset, float dlStartTime) : m_WaveData(dlStartPosX, dlAmplitude, dlFrequency, dlOffset, dlStartTime) {};
 
 	WaveData& GetWave() { return m_WaveData; }
 
 	void SetAmplitude(double dlAmplitude) { m_WaveData.Amp = dlAmplitude; }
 	void SetFrequency(double dlFrequency) { m_WaveData.Freq = dlFrequency; }
 	void SetOffset(double dlOffset) { m_WaveData.Offset = dlOffset; }
-	void SetStartTime(double dlStartTime) { m_WaveData.m_dlTimeDiff = ImGui::GetTime() - dlStartTime; }
+	void SetStartTime(double dlStartTime) { m_WaveData.m_dlTimeDiff = dlStartTime; }
 
-	void ResetTime() { m_WaveData.m_dlTimeDiff = 0; }
+	double GetFrequency() { return m_WaveData.Freq; }
+	double GetAmplitude() { return m_WaveData.Amp; }
+
+	void ResumeTime() { m_WaveData.m_dlTimeDiff = ImGui::GetTime() - m_dlStartTime; }
+	void ResetTime() { m_WaveData.m_dlTimeDiff = 0; m_dlStartTime = ImGui::GetTime(); }
+
 private:
 	double m_dlStartTime;
 	WaveData m_WaveData;
 };
-
